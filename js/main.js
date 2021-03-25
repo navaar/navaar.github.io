24cb481260686e543b661e80abf2ce9413aa7171
navaar <lofimoods98@gmail.com>
2021-03-25 18:02:16 +0530

    hj

--- js/main.js
+++ js/main.js
@@ -242,10 +242,10 @@
 
 	  	});
 
-	  	// $('#fh5co-hero .flexslider .slides > li').css('height', $(window).height());	
-	  	// $(window).resize(function(){
-	  	// 	$('#fh5co-hero .flexslider .slides > li').css('height', $(window).height());	
-	  	// });
+	   $('#fh5co-hero .flexslider .slides > li').css('height', $(window).height());	
+	    $(window).resize(function(){
+	   	$('#fh5co-hero .flexslider .slides > li').css('height', $(window).height());	
+	    });
 
 	};
 
